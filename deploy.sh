d541ef3eb5d71d70a82d21829c1ff7a2021b7f3c
lmyro <camogie@live.co.uk>
2022-07-25 13:03:42 +0100

    added sh file

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env/env bash
-sudo apt update && sudo apt install nodejs npm
-# Install pm2 which is a production process manager for Node.js with a built-in load balancer.
-sudo npm install -g pm2
-# Stop any instance of our application currently running
-pm2 stop example_app
-# Change Directory into folder where application is downloaded
-cd ExampleApp/
-# Install dependencies
-npm install
-# Start with pm2
-pm2 start ./bin/www --name example_app
+
+#!/usr/bin/env bash
+ sudo apt update && sudo apt install nodejs npm
+ # install pm2 which is production process manager for nodejs w/ built in load balancer
+ sudo npm install -g pm2
+ # stop any instance of application running
+ pm2 stop example_app
+ # change directory into folder where application is downloaded
+ cd ExampleApp/
+ # install app dependencies
+ npm install
+ # start app w/ process name example_app
+ pm2 start ./bin/www --name example_app 
