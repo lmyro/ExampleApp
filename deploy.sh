433039e31d030673dd3af6fc3646d88e3b5f6363
lmyro <camogie@live.co.uk>
2022-07-25 13:36:23 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 
 #!/usr/bin/env bash
- sudo apt update && sudo apt install nodejs npm
+sudo apt update && sudo apt install nodejs npm
  # install pm2 which is production process manager for nodejs w/ built in load balancer
 npm install -g pm2
  # stop any instance of application running
- pm2 stop example_app
+pm2 stop example_app
  # change directory into folder where application is downloaded
- cd ExampleApp/
+cd ExampleApp/
  # install app dependencies
 sudo npm install
  # start app w/ process name example_app
- pm2 start ./bin/www --name example_app 
+pm2 start ./bin/www --name example_app 
